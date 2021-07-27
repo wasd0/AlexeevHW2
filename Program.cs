321303cb2027c814178b9449aca717578a8792f6
Shvedov Kirill <84603952+wasd0@users.noreply.github.com>
2021-07-28 00:59:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexeevHW2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Figure[] arr = {new Circle(), new Ellipse(), new Trigon(), new Tetragon(), new Pentagon()};
+
+            foreach (Figure figure in arr)
+            {
+                if (figure is FigureWithoutCorners)
+                    Console.WriteLine("Оно не умеет углов");
+                if (figure is Polygon)
+                    Console.WriteLine("В нём много углов");
+                PrintInfo(figure);
+            }
+
+            Console.ReadKey();
+        }
+        static void PrintInfo(Figure figure)
+        {
+            Console.WriteLine($"Название фигуры {figure.Name}");
+            figure.HowToDraw();
+            Console.WriteLine(new String('-', 35));
+        }
+}
+    abstract class Figure
+    {
+        public abstract string Name { get; }
+        public abstract void HowToDraw();
+    }
+    abstract class FigureWithoutCorners : Figure
+    {
+        public override void HowToDraw()
+        {
+            Console.WriteLine($"Рисуем {Name} без углов");
+        }
+    }
+
+    abstract class Polygon : Figure
+    {
+        public override void HowToDraw()
+        {
+            Console.WriteLine("Рисуем много углов");
+        }
+    }
+    class Circle : FigureWithoutCorners
+    {
+        public override string Name { get; } = "Круг";
+        public override void HowToDraw()
+        {
+            Console.WriteLine($"Рисуем {Name} без углов");
+        }
+    }
+    class Ellipse : FigureWithoutCorners
+    {
+        public override string Name { get; } = "Овал";
+        public override void HowToDraw()
+        {
+            Console.WriteLine($"Рисуем вытянутый {Name}");
+        }
+    }
+    class Trigon : Polygon
+    {
+        public override string Name { get; } = "Треугольник";
+        public override void HowToDraw()
+        {
+            Console.WriteLine($"Рисуем {Name}");
+        }
+    }
+    class Tetragon : Polygon
+    {
+        public override string Name { get; } = "Четырёхугольник";
+        public override void HowToDraw()
+        {
+            Console.WriteLine($"Рисуем {Name}");
+        }
+    }
+    class Pentagon : Polygon
+    {
+        public override string Name { get; } = "Пятиугольник";
+        public override void HowToDraw()
+        {
+            Console.WriteLine($"Рисуем {Name}");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("AlexeevHW2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlexeevHW2")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5f0d2dd0-2d16-4ca0-bd27-2ae6b366ab3a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
